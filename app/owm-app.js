350c137b6c30f35ba1ba593fd2ef0f9d0738643b
Ben Boyle <benjamins.boyle@gmail.com>
2014-07-07 17:38:59 +1000

    basic routine

---
+++ app/owm-app.js
@@ -0,0 +1,25 @@
+angular.module( 'OWMApp', [ 'ngRoute' ] )
+
+	.config(function( $routeProvider ) {
+		$routeProvider
+			.when( '/', {
+				templateUrl: 'home.html',
+				controller: 'HomeCtrl'
+			})
+
+			.when( '/cities/:city', {
+				templateUrl: './city.html',
+				controller: 'CityCtrl'
+			})
+		;
+	})
+
+
+	.controller( 'HomeCtrl', function( $scope ) {
+		// empty for now
+	})
+
+	.controller( 'CityCtrl', function( $scope, $routeParams ) {
+		$scope.city = $routeParams.city;
+	})
+;
