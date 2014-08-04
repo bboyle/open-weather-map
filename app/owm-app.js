8a1659df7a613f9c7691ebc9525ac0c18c4f3281
Ben Boyle <benjamins.boyle@gmail.com>
2014-08-04 12:53:13 +1000

    whitespace
    adjust indenting

--- app/owm-app.js
+++ app/owm-app.js
@@ -3,63 +3,63 @@ angular.module( 'OWMApp', [
 	'ngAnimate'
 ])
 
-	.value( 'owmCities', [
-		'New York',
-		'Dallas',
-		'Chicago',
-	])
+.value( 'owmCities', [
+	'New York',
+	'Dallas',
+	'Chicago',
+])
 
-	.config(function( $routeProvider ) {
-		$routeProvider
-			.when( '/', {
-				templateUrl: 'home.html',
-				controller: 'HomeCtrl as home'
-			})
+.config(function( $routeProvider ) {
+	$routeProvider
+		.when( '/', {
+			templateUrl: 'home.html',
+			controller: 'HomeCtrl as home'
+		})
 
-			.when( '/error', { template: '<p>Error: Page not found</p>' })
+		.when( '/error', { template: '<p>Error: Page not found</p>' })
 
-			.when( '/cities/:city', {
-				templateUrl: 'city.html',
-				controller: 'CityCtrl',
-				resolve: {
-					city: function( owmCities, $route, $location ) {
-						var city = $route.current.params.city;
+		.when( '/cities/:city', {
+			templateUrl: 'city.html',
+			controller: 'CityCtrl',
+			resolve: {
+				city: function( owmCities, $route, $location ) {
+					var city = $route.current.params.city;
 
-						if ( owmCities.indexOf( city ) === -1 ) {
-							$location.path( '/error' );
-							return;
-						}
-						return city;
+					if ( owmCities.indexOf( city ) === -1 ) {
+						$location.path( '/error' );
+						return;
 					}
+					return city;
 				}
-			})
+			}
+		})
 
-			.otherwise({ redirectTo: '/error' })
-		;
-	})
+		.otherwise({ redirectTo: '/error' })
+	;
+})
 
-	.run(function( $rootScope, $location, $timeout ) {
-		$rootScope.$on( '$routeChangeError', function() {
-			$location.path( '/error' );
-		});
+.run(function( $rootScope, $location, $timeout ) {
+	$rootScope.$on( '$routeChangeError', function() {
+		$location.path( '/error' );
+	});
 
-		$rootScope.$on( '$routeChangeStart', function() {
-			$rootScope.isLoading = true;
-		});
-		$rootScope.$on( '$routeChangeSuccess', function() {
-			// simulate delay to implement animations
-			$timeout(function() {
-				$rootScope.isLoading = false;
-			}, 1000 );
-		});
-	})
+	$rootScope.$on( '$routeChangeStart', function() {
+		$rootScope.isLoading = true;
+	});
+	$rootScope.$on( '$routeChangeSuccess', function() {
+		// simulate delay to implement animations
+		$timeout(function() {
+			$rootScope.isLoading = false;
+		}, 1000 );
+	});
+})
 
 
-	.controller( 'HomeCtrl', function( $scope ) {
-		this.welcomeMessage = 'Welcome home';
-	})
+.controller( 'HomeCtrl', function( $scope ) {
+	this.welcomeMessage = 'Welcome home';
+})
 
-	.controller( 'CityCtrl', function( $scope, city ) {
-		$scope.city = city;
-	})
+.controller( 'CityCtrl', function( $scope, city ) {
+	$scope.city = city;
+})
 ;
